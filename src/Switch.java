1a4b9ad8a79afe4071a1d0bd937e5bdfcf37c970
Zhuosong Leng <zhuosong.leng@bsu.edu>
2026-01-27 15:12:25 -0500

    Mainly Finished the switch class
    Finished the packets receiver and sender functions

--- src/Switch.java
+++ src/Switch.java
@@ -1,33 +1,56 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Switch {
     public static void main(String[] args) throws Exception {
-        Map<String, String[]> table = new HashMap<>();
+        Map<String, InetSocketAddress> table = new HashMap<>();
         String ID = args[0];
         Parser parser = new Parser(ID);
         int portNum = parser.getPortNum();
-        InetAddress IP = InetAddress.getByName(parser.getAddress());
+        InetSocketAddress[] neighbors = parser.getNeighbors();
+        DatagramSocket socket = new DatagramSocket(portNum);
 
         while (true) {
-            String frame = receivePacket(portNum);
-            sendPacket(frame);
+            DatagramPacket packetReceived = receivePacket(socket);
+            String frame = new String(packetReceived.getData(), 0, packetReceived.getLength());
+
+            InetSocketAddress sender = new InetSocketAddress(packetReceived.getAddress(), packetReceived.getPort());
+
+            String[] info = frame.split(":");
+            String srcMAC = info[0];
+            String dstMAC = info[1];
+
+            table.putIfAbsent(srcMAC, sender);
+            if (table.containsKey(dstMAC)) {
+                InetSocketAddress target = table.get(dstMAC);
+                sendPacket(socket, frame, target);
+            }
+            else {
+                for (int i = 0; i < neighbors.length; i++) {
+                    if (!neighbors[i].equals(sender)) {
+                        sendPacket(socket, frame, neighbors[i]);
+                    }
+                }
+            }
         }
     }
 
-    private static void sendPacket(String frame) throws Exception {
+    private static void sendPacket(DatagramSocket socket, String frame, InetSocketAddress target) throws Exception {
+        byte[] data = frame.getBytes();
+
+        DatagramPacket packetSent = new DatagramPacket(data, data.length, target.getAddress(), target.getPort());
+        socket.send(packetSent);
     }
 
-    private static String receivePacket(int portNum) throws Exception {
-        DatagramSocket socket = new DatagramSocket(portNum);
+    private static DatagramPacket receivePacket(DatagramSocket socket) throws Exception {
         byte[] buffer = new byte[1024];
 
         DatagramPacket packetReceived = new DatagramPacket(buffer, buffer.length);
         socket.receive(packetReceived);
 
-        return new String(packetReceived.getData(), 0, packetReceived.getLength());
+        return packetReceived;
     }
 }
