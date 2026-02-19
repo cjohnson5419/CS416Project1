c42fee45ed15a950110073daa2e1e90aaa417355
Zhuosong Leng <zhuosong.leng@bsu.edu>
2026-02-19 15:21:24 -0500

    Update Device.java

--- src/Device.java
+++ src/Device.java
@@ -1,31 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Device {
     private String id;
     private String realIP;
     private int realPort;
     private String virtualIP;
     private String gateway;
-    private List<String> virtualPorts;
 
-    public Device(String id, String realIP, int realPort) {
+    public Device(String id, String realIP, int realPort, String virtualIP, String gateway) {
         this.id = id;
         this.realIP = realIP;
         this.realPort = realPort;
-        this.virtualPorts = new ArrayList<>();
+        this.virtualIP = virtualIP;
+        this.gateway = gateway;
     }
 
-    public String getId()            { return id; }
-    public String getRealIP()        { return realIP; }
-    public int getRealPort()         { return realPort; }
+    public String getId() {
+        return id;
+    }
 
-    public String getVirtualIP()     { return virtualIP; }
-    public void setVirtualIP(String v) { this.virtualIP = v; }
+    public String getRealIP() {
+        return realIP;
+    }
 
-    public String getGateway()       { return gateway; }
-    public void setGateway(String g) { this.gateway = g; }
+    public int getRealPort() {
+        return realPort;
+    }
 
-    public List<String> getVirtualPorts()          { return virtualPorts; }
-    public void setVirtualPorts(List<String> vp)   { this.virtualPorts = vp; }
+    public String getVirtualIP() {
+        return virtualIP;
+    }
+
+    public String getGateway() {
+        return gateway;
+    }
 }
