051e20b8e2a00216381e890e3c6365a6bd739d17
Zhuosong Leng <zhuosong.leng@bsu.edu>
2026-03-18 00:58:23 -0400

    update the RoutingTables and config_file so that now is able to set initial table

--- src/RoutingTables.java
+++ src/RoutingTables.java
@@ -1,26 +1,39 @@
-import java.io.PrintStream;
+import javax.crypto.spec.PSource;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
-public class RoutingTables {
+public class RoutingTable {
+    private Map<String, String> forwardingTable = new HashMap<>();
+    private Parser parser;
+    private String ID;
 
-    String[] net;
-    Map<String, String> fowardingTable = new HashMap<>();
-    Map<String, InetSocketAddress> neighbors = new HashMap<>();
-
-    public RoutingTables(Parser parser) {
-        neighbors = parser.getNeighbors();
-        String[] netID = parser.getVirtualIP().split(",");
-        for(int i = 0; i < netID.length; i++) {
-            System.out.println(netID[i]);
-        }
-
+    public RoutingTable(Parser parser, String ID) {
+        this.ID = ID;
+        this.parser = parser;
+        getTable();
     }
 
-    public void printTable() {
-        System.out.println(net[0]);
+    private void getTable() {
+        String destinations = null;
+        Set<Map.Entry<String, InetSocketAddress>> neighbors = parser.getNeighbors().entrySet();
+        String[] virtualIPInfo = parser.getDevice(ID).getVirtualIP().split(",");
+
+        for (Map.Entry<String, InetSocketAddress> neighbor : neighbors) {
+            String neighborVIPInfo = parser.getDevice(neighbor.getKey()).getVirtualIP();
+
+            for (int i = 0; i < virtualIPInfo.length; i++) {
+                String subNet = virtualIPInfo[i].split("\\.")[0];
+
+                if (neighborVIPInfo == null || neighborVIPInfo.contains(subNet)) {
+                    destinations = subNet;
+                    break;
+                }
+            }
+            forwardingTable.putIfAbsent(destinations, neighbor.getKey() + neighbor.getValue());
+        }
+        System.out.println(forwardingTable.toString());
     }
 }
